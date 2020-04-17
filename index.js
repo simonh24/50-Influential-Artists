67467d003731f5cd0fb5a6fb4bc273161eafd0d4
Simon <simonh72498@gmail.com>
2020-04-17 13:01:03 -0400

    finished less/css and html portion, began working on js portion.

--- index.js
+++ index.js
@@ -208,7 +208,8 @@ const artists = [
 (1) Name of the first artist in the array
 (2) Bio of the third artist in the array */
 
-
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
@@ -222,8 +223,8 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the inventory and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(id, name) {
-    /* code here */
+function getArtistByIndex(arr, ind) {
+  return `The artist at index ${ind} is ${arr[ind].name}`;
   }
   
   /**
